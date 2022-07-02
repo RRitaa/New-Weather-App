b16b2ca0d0594d350d7b349762283111d177dbe0
RRitaa <108019285+RRitaa@users.noreply.github.com>
2022-07-02 17:13:37 -0400

    Added search engine

--- sccrript.js
+++ sccrript.js
@@ -43,8 +43,21 @@ function displayTemprature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "b26d34366f12efd35f2df5f40edbeeff";
-let city = "Toronto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "b26d34366f12efd35f2df5f40edbeeff";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemprature);
+}
 
-axios.get(apiUrl).then(displayTemprature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+
+search ("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
