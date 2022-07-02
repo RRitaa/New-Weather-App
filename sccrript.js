8e699fc41c613a5effb4afb9a5a11e20a5369d0f
RRitaa <108019285+RRitaa@users.noreply.github.com>
2022-07-02 16:23:56 -0400

    Removed console log

--- sccrript.js
+++ sccrript.js
@@ -22,13 +22,13 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTemprature(response) {
-  console.log(response.data.main.temp);
   let tempratureElement = document.querySelector("#temprature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.queryCommandValue("#wind");
   let dateElement = document.querySelector("#date");
+  
   tempratureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
