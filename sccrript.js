e070af6cf4efd99a17c527e289e7bd1f1a6b9de3
RRitaa <108019285+RRitaa@users.noreply.github.com>
2022-07-02 16:22:45 -0400

    updated date and time

--- sccrript.js
+++ sccrript.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours > 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemprature(response) {
   console.log(response.data.main.temp);
   let tempratureElement = document.querySelector("#temprature");
@@ -5,11 +28,13 @@ function displayTemprature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.queryCommandValue("#wind");
+  let dateElement = document.querySelector("#date");
   tempratureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b26d34366f12efd35f2df5f40edbeeff";
