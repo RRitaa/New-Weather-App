354cf26844631c9028ae5a4390b5eec50703057b
RRitaa <108019285+RRitaa@users.noreply.github.com>
2022-07-02 15:01:50 -0400

    updated current temperature, wind and humidity

--- sccrript.js
+++ sccrript.js
@@ -3,9 +3,13 @@ function displayTemprature(response) {
   let tempratureElement = document.querySelector("#temprature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.queryCommandValue("#wind");
   tempratureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "b26d34366f12efd35f2df5f40edbeeff";
